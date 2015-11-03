6313e52173e778b3a168b542f359072eaaafec31
Huy Nguyen Huu <huuhuy@gmail.com>
2015-11-03 14:08:02 +0700

    Add view count, saved count, and address source

--- words.js
+++ words.js
@@ -12,28 +12,71 @@ var datas = [];
 for(i = 0; i < wordCount; i++) {
   var key = localStorage.key(i);
   var wordObj = localStorage.getObject(key);
-  datas.push({
-    text: wordObj.text,
-    date: moment(wordObj.date).format()
-  });
-  var now = moment();
-  datas = _.sortBy(datas, function(item){
-    return now - item.date;
-  })
+  wordObj.date = moment(wordObj.date).format();
+  if(wordObj.viewCount == undefined || wordObj.viewCount == null) {
+    wordObj.viewCount = 0;
+  }
+
+  if(wordObj.savedCount == undefined || wordObj.savedCount == null) {
+    wordObj.savedCount = 0;
+  }
+
+  datas.push(wordObj);
+}
+
+var now = moment();
+datas = _.sortBy(datas, function(item){
+  return now - item.date;
+});
+
+function updateViewCount(word){
+  var wordObj = localStorage.getObject(word);
+  wordObj.viewCount = (wordObj.viewCount == undefined ? 0 : wordObj.viewCount) + 1;
+  localStorage.setObject(word, wordObj);
+
+  var id = wordObj.text.replace(/\s+/g, '_');
+  $('#viewCount_'+id).html(wordObj.viewCount);
+  return wordObj.viewCount;
 }
 
 //var dicUrl = "http://www.oxforddictionaries.com/definition/english/";
 var dicUrl = 'http://www.oxforddictionaries.com/search/?multi=1&dictCode=english&q=';
+var dynamicTable = null;
 $(function(){
-  $('#my-final-table').dynatable({
+  dynamicTable = $('#my-final-table').dynatable({
     dataset: {
       records: datas
     },
     writers: {
       _rowWriter: function(rowIndex, record, columns, cellWriter) {
-        var textWithLink = "<a target='_blank' href='" + dicUrl + record.text +"'>" + record.text + "</a>";
-        return '<tr><td style="text-align: left;">' + textWithLink + '</td><td style="text-align: left;">' + record.date + '</td></tr>';
+        var id = record.text.replace(/\s+/g, '_');
+        var source = '<td>&nbsp;</td>';
+        var savedCount = '<td>&nbsp;</td>';
+        var viewCount = '<td id="viewCount_' + id + '">&nbsp;</td>';
+        if(record.url) {
+          source = '<td><a target="_blank" href="' + record.url +'">source</a></td>';
+        }
+        if(record.savedCount) {
+          savedCount = '<td>' + record.savedCount + '</td>';
+        }
+        if(record.viewCount) {
+          viewCount = '<td id="viewCount_' + id + '">' + record.viewCount + '</td>';
+        }
+        var textWithLink = "<a target='_blank' id='text_" + id + "' href='" + dicUrl + record.text +"'>" + record.text + "</a>";
+        return '<tr><td style="text-align: left;">' + textWithLink + '</td><td style="text-align: left;">' + record.date + '</td>' + savedCount + viewCount + source + '</tr>';
       }
     },
+  }).data('dynatable');
+
+  $('body').on('click', "a[id^=text_]", function () {
+    var text =($(this).text());
+    var viewCount = updateViewCount(text);
+    dynamicTable.settings.dataset.originalRecords = _.map(dynamicTable.settings.dataset.originalRecords, function(it){
+      if(it.text == text){
+        it.viewCount = viewCount;
+      }
+      return it;
+    });
+    dynamicTable.process();
   });
 });

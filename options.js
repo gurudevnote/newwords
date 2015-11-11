feb322ea00a05415de5db0fae30068e0561245ba
Huy Nguyen Huu <huuhuy@gmail.com>
2015-11-12 00:10:21 +0700

    Add option page to synchonize data from local storage to chrome storage

---
+++ options.js
@@ -0,0 +1,25 @@
+$(function(){
+	$('#synchonize').click(function(){
+		synchronizeChrome();
+	});
+
+	$('#getAllfromChromeStorage').click(function(){
+		chrome.storage.sync.get(null, function(items) {
+			//console.dir(items);
+		    //var allKeys = Object.keys(items);
+		    //console.dir(allKeys);
+		    var numberUpdated = 0;
+		    var updatedKeys = [];
+		    _.map(items, function(value, key){
+		    	//console.log(key);
+		    	var localValue = localStorage.getObject(key);
+		    	if(localValue == undefined || localValue == null) {
+		    		localStorage.setItem(key, value);
+		    		updatedKeys.push(key);
+		    		numberUpdated ++;
+		    	}
+		    })
+		    alert('number items is imported from chrome storage to local storage is ' + numberUpdated + '\n' + updatedKeys.join('\n'));
+		});
+	});
+});
--- storage.js
+++ storage.js
@@ -6,3 +6,17 @@ Storage.prototype.getObject = function(key) {
   var value = this.getItem(key);
   return value && JSON.parse(value);
 }
+
+function synchronizeChrome(){
+	var wordCount = localStorage.length;
+	var listWords = {};
+	for(i = 0; i < wordCount; i++) {
+	  var key = localStorage.key(i);
+	  var wordObjStringification = localStorage.getItem(key);
+	  listWords[key] = wordObjStringification;
+	}
+	//console.dir(listWords);
+	chrome.storage.sync.set(listWords, function(){
+		alert('synchonize sucessfull');
+	});
+}

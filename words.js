7f332b72b68072ac3a7d653a35f14e3a0373473c
Huy Nguyen Huu <huuhuy@gmail.com>
2015-11-11 14:31:25 +0700

    replextor code

--- words.js
+++ words.js
@@ -1,12 +1,3 @@
-Storage.prototype.setObject = function(key, value) {
-  this.setItem(key, JSON.stringify(value));
-}
-
-Storage.prototype.getObject = function(key) {
-  var value = this.getItem(key);
-  return value && JSON.parse(value);
-}
-
 var wordCount = localStorage.length;
 var datas = [];
 var events = [];

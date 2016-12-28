397268e0959299d47f7f86eaa7b8e9c6f1af2803
Huy Nguyen Huu <huuhuy@gmail.com>
2016-12-28 15:50:47 +0700

    add firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyABYTFst6nxNJ1vjMKb1lUaa8j6bVJ86rU",
+    authDomain: "newwords-d0e1c.firebaseapp.com",
+    databaseURL: "https://newwords-d0e1c.firebaseio.com",
+    storageBucket: "newwords-d0e1c.appspot.com",
+    messagingSenderId: "318144735480"
+};
+firebase.initializeApp(config);
+var firebaseDatabase = firebase.database();

ad9bcf22e152eae4b3d7c5fe7dc4eb8815cab6b4
Huy Nguyen Huu <huuhuy@gmail.com>
2017-01-09 13:58:54 +0700

    give more option for show number item per paper

--- words.js
+++ words.js
@@ -171,7 +171,8 @@ $(function(){
     events = StorageApi.getAllWordForDisplayingOnCalendar(datas);
     dynamicTable = $('#my-final-table').dynatable({
       dataset: {
-        records: datas
+        records: datas,
+        perPageOptions: [10,20,50,100,200,400,600,1000, 10000]
       },
       writers: {
         _rowWriter: function(rowIndex, record, columns, cellWriter) {
